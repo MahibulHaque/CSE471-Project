4ef7d70ccb5ba413bea27a16362a5cef281ac1c1
Md Mahibul Haque <mahibulhaque2000@gmail.com>
2022-05-04 11:29:14 +0600

    Fix error handaling in user-auth api route

--- pages/api/auth/user/user-auth.js
+++ pages/api/auth/user/user-auth.js
@@ -1,31 +1,25 @@
 import User from "../../../../models/User";
 import jwt from "jsonwebtoken";
 import {
-  checkCookies,
   getCookie,
-  getCookies,
-  removeCookies,
 } from "cookies-next";
 
-export default async function handler(req, res){
+export default async function handler(req, res) {
   const token = getCookie("user-token", { req, res });
   if (token) {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     const obj = await User.findOne({ _id: verified.id });
     if (obj) {
-      return res
-        .status(200)
-        .json({
-          name: obj.name,
-          email: obj.email,
-          username: obj.username,
-          image: obj.imageUrl,
-        });
+      return res.status(200).json({
+        name: obj.name,
+        email: obj.email,
+        username: obj.username,
+        image: obj.imageUrl,
+      });
     } else {
       return res.status(400).send({ error: "User does not exist" });
     }
   } else {
-    res.status(400);
-    res.end("Token not found");
+    return res.status(400).send({ error: "Token not found" });
   }
-};
+}
